ef8d80ee2054422ed4b884f888f88fd765030ffd
Reza 1290 <reza.muktasib@gmail.com>
2024-09-07 11:42:33 +0900

    fix: image

--- app/components/StackComponent.tsx
+++ app/components/StackComponent.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 interface StackComponentProps {
@@ -23,7 +24,7 @@ const StackComponent: React.FC<StackComponentProps> = ({ id, name, imageUrl, fir
             <div className="text-white font-bold peer-checked:block hidden bg-blue-400 hover:bg-blue-500 h-10 w-40 absolute rounded-md rotate-[-5deg] -top-[5.2rem] transition-opacity ease-in-out delay-150 duration-300">
                 <p className='flex items-center justify-center h-10'>{type} Work</p>
             </div>
-            <img className={`shrink-0 w-40 h-40 rounded-md shadow-xl bg-white ${first && !last ? 'mask-first' : !first && last ? 'mask-last' : ''}`} src={imageUrl} alt="Radio Thumbnail" />
+            <Image className={`shrink-0 w-40 h-40 rounded-md shadow-xl bg-white ${first && !last ? 'mask-first' : !first && last ? 'mask-last' : ''}`} src={imageUrl} alt="Radio Thumbnail" />
         </label>
     );
 };
