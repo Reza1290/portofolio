a8516c3fdfcb447359568e33006e9e68638eb82e
Reza 1290 <reza.muktasib@gmail.com>
2024-09-08 19:51:16 +0900

    add: footer

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -1,9 +1,11 @@
-
-const Labs = () => {
-    return (
+export function generateStaticParams() {
+    return [{ id: '1' }, { id: '2' }, { id: '3' }]
+  }
+   
+  export default function Page({ params }: { params: { id: string } }) {
+    const { id } = params
+    return(
         <>
         </>
     )
-}
-
-export default Labs
+  }
