3d27ff6fe29ed676a79a5d7d7d6d82bfa8388dd7
Reza 1290 <reza.muktasib@gmail.com>
2024-09-19 03:43:56 +0900

    add: pdf

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -19,9 +19,9 @@ const NavBar: React.FC<NavBarProps> = () => {
                 <a href="/" className="text-4xl font-bold select-none">Reza</a>
             </div>
             <div className="bg-blue-600 rounded-md h-10 w-28 flex justify-center items-center group-hover">
-                <button className="flex rounded-md bg-blue-400 w-full justify-center h-full items-center translate-y-[-3px] font-medium hover:bg-blue-500 transition-colors ease-in">
+                <a href="/assets/cv.pdf" download='Muhamad Reza Muktasib - Resume' className="flex rounded-md bg-blue-400 w-full justify-center h-full items-center translate-y-[-3px] font-medium hover:bg-blue-500 transition-colors ease-in">
                     Start
-                </button>
+                </a>
             </div>
         </nav>
     )
