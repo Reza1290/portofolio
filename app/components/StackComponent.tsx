b067e8cd83c372ffff516fa7a3a1ea6bba684e12
Reza 1290 <reza.muktasib@gmail.com>
2024-09-20 22:45:04 +0900

    fix: assets

--- app/components/StackComponent.tsx
+++ app/components/StackComponent.tsx
@@ -21,8 +21,8 @@ const StackComponent: React.FC<StackComponentProps> = ({ id, name, imageUrl, fir
             <div className="text-black peer-checked:block hidden bg-blue-600 h-10 w-40 max-sm:w-20 max-sm:h-5 absolute rounded-md rotate-[-5deg] -top-20 transition-opacity ease-in-out delay-150 duration-300">
                 
             </div>
-            <a href={`/labs?stack=${type.charAt(0).toUpperCase() + type.slice(1).toLowerCase()}`} className="text-white font-bold peer-checked:block hidden bg-blue-400 hover:bg-blue-500 h-10 max-sm:h-5 w-40 max-sm:w-20  absolute rounded-md rotate-[-5deg] -top-[5.2rem] transition-opacity ease-in-out delay-150 duration-300">
-                <p className='flex items-center justify-center h-10 max-sm:h-5'>{type.charAt(0).toUpperCase() + type.slice(1).toLowerCase()} Work</p>
+            <a href={`/labs?stack=${type}`} className="text-white font-bold peer-checked:block hidden bg-blue-400 hover:bg-blue-500 h-10 max-sm:h-5 w-40 max-sm:w-20  absolute rounded-md rotate-[-5deg] -top-[5.2rem] transition-opacity ease-in-out delay-150 duration-300">
+                <p className='flex items-center justify-center h-10 max-sm:h-5'>{type} Work</p>
             </a>
             <Image width={'160'} height={'160'} className={`shrink-0 w-40 max-sm:w-20 max-sm:h-[5rem] h-40 rounded-md shadow-xl bg-white ${first && !last ? 'mask-first' : !first && last ? 'mask-last' : ''}`} src={"/" +imageUrl.toLowerCase()} alt="Radio Thumbnail" />
         </label>
